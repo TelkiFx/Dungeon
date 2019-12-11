a4eb884b51d6a337c2fc1ae8700a8cfba6f1b3d9
Cameron <camtredoux@gmail.com>
2019-12-10 22:31:00 -0700

    Gave Board class the responsibility of placing a Player

--- src/dungeon/board/Board.java
+++ src/dungeon/board/Board.java
@@ -1,7 +1,9 @@
-package dungeon;
+package dungeon.board;
 
 public class Board {
 
+    private int x;
+    private int y;
     private int width;
     private int height;
     private String[][] board;
@@ -27,10 +29,29 @@ public class Board {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
-    public String[][] getBoard() {
-        return board;
+    public boolean placePlayer(int x, int y) {
+        build();
+
+        try {
+            board[y][x] = "@";
+            this.x = x;
+            this.y = y;
+            return true;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            board[this.y][this.x] = "@";
+            return false;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
 }
