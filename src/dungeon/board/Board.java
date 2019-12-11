e0bde605a343c7520b7d55980810938003395e78
Cameron <camtredoux@gmail.com>
2019-12-10 23:43:59 -0700

    Moved placePlayer() to CoordinateSystem

--- src/dungeon/board/Board.java
+++ src/dungeon/board/Board.java
@@ -2,8 +2,6 @@ package dungeon.board;
 
 public class Board {
 
-    private int x;
-    private int y;
     private int width;
     private int height;
     private String[][] board;
@@ -15,6 +13,7 @@ public class Board {
     }
 
     public void build() {
+        System.out.print("\033[H\033[2J");
         for (int y = 0; y < height; ++y) {
             for (int x = 0; x < width; ++x) {
                 board[y][x] = ".";
@@ -32,43 +31,16 @@ public class Board {
         System.out.println();
     }
 
-    public void placePlayer(String moves) {
-        for (char move : moves.toCharArray()) {
-            switch (move) {
-                case 'w':
-                    if (y - 1 >= 0) {
-                        y -= 1;
-                    }
-                    break;
-                case 'a':
-                    if (x - 1 >= 0) {
-                        x -= 1;
-                    }
-                    break;
-                case 's':
-                    if (y + 1 < height) {
-                        y += 1;
-                    }
-                    break;
-                case 'd':
-                    if (x + 1 < width) {
-                        x += 1;
-                    }
-                    break;
-                default:
-                    System.out.println("Not a valid move");
-            }
-        }
-        build();
-        board[y][x] = "@";
+    public int getWidth() {
+        return width;
     }
 
-    public int getX() {
-        return x;
+    public int getHeight() {
+        return height;
     }
 
-    public int getY() {
-        return y;
+    public String[][] getBoard() {
+        return board;
     }
 
 }
