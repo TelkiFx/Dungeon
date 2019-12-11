23155f798fcf82985b2673cb7b032b7029990f57
Cameron <camtredoux@gmail.com>
2019-12-11 00:36:44 -0700

    Broken attempt at adding unique vampire locations

--- src/dungeon/board/CoordinateSystem.java
+++ src/dungeon/board/CoordinateSystem.java
@@ -1,17 +1,33 @@
 package dungeon.board;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 public class CoordinateSystem {
 
-    private Board board;
     private int x;
     private int y;
     private int width;
     private int height;
+    private int numVampires;
 
-    public CoordinateSystem(Board board) {
+    private List<Integer> playerCoords = new ArrayList<Integer>();
+
+    private Random random = new Random();
+    private Set<List<Integer>> vampireLocations = new HashSet<List<Integer>>();
+    private Board board;
+
+    public CoordinateSystem(Board board, int numVampires) {
         this.board = board;
         this.width = this.board.getWidth();
         this.height = this.board.getHeight();
+        this.numVampires = numVampires;
+
+        placePlayer("");
+        placeVampires();
     }
 
     public void placePlayer(String moves) {
@@ -41,13 +57,41 @@ public class CoordinateSystem {
         }
         board.build();
         board.getBoard()[y][x] = "@";
+        playerCoords.clear();
+        playerCoords.add(x);
+        playerCoords.add(y);
     }
 
-    public int getX() {
+    public void placeVampires() {
+        vampireLocations.clear();
+        while (vampireLocations.size() < numVampires) {
+            int vampireX = random.nextInt(10);
+            int vampireY = random.nextInt(10);
+            List<Integer> vampireCoords = new ArrayList<Integer>();
+            vampireCoords.add(vampireX);
+            vampireCoords.add(vampireY);
+            vampireLocations.add(vampireCoords);
+
+            if (!vampireLocations.contains(playerCoords)) {
+                vampireLocations.add(vampireCoords);
+                board.getBoard()[vampireY][vampireX] = "v";
+            }
+        }
+    }
+
+    public Set<List<Integer>> getVampireLocations() {
+        return vampireLocations;
+    }
+
+    public List<Integer> getPlayerCoords() {
+        return playerCoords;
+    }
+
+    public int getPlayerX() {
         return x;
     }
 
-    public int getY() {
+    public int getPlayerY() {
         return y;
     }
 
