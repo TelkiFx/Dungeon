5edd232117bd846339f929ab339bc5de2e4cf171
Cameron <camtredoux@gmail.com>
2019-12-11 13:28:42 -0700

    Added toString() for cleaner printing

--- src/dungeon/board/Points.java
+++ src/dungeon/board/Points.java
@@ -21,6 +21,11 @@ public class Points {
     }
 
     @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (o == null) {
             return false;
